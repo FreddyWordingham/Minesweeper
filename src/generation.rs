28a5bba066832942a03252d1200aac1679013182
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-23 22:55:34 +0100

    Moving to loopless stages

--- src/generation.rs
+++ src/generation.rs
@@ -1,23 +1,28 @@
 use bevy::{
     log,
-    prelude::{App, Plugin, ResMut, State, SystemSet},
+    prelude::{App, Commands, Plugin},
 };
-
-use crate::game::GameState;
+use iyes_loopless::prelude::ConditionSet;
 
 pub struct GenerationPlugin;
 
 impl Plugin for GenerationPlugin {
     fn build(&self, app: &mut App) {
         app.add_system_set(
-            SystemSet::on_enter(GameState::Generating).with_system(Self::generate_board),
+            ConditionSet::new()
+                .run_unless_resource_exists::<Board>()
+                .with_system(Self::generate_board)
+                .into(),
         );
     }
 }
 
 impl GenerationPlugin {
-    fn generate_board(mut state: ResMut<State<GameState>>) {
+    fn generate_board(mut commands: Commands) {
         log::info!("Generating board!");
-        state.set(GameState::Menu).unwrap();
+
+        commands.insert_resource(Board {})
     }
 }
+
+struct Board {}
