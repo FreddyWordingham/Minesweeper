3bd63b52b4f2517080ad74b1aa6a4fb7ee96dff8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-23 18:05:52 +0100

    Removed unused imports

--- src/loading.rs
+++ src/loading.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::{App, Plugin};
+use bevy::prelude::{App, AssetServer, Font, Handle, HandleUntyped, Image, Plugin, World};
 use bevy_asset_loader::{AssetCollection, AssetLoader};
 use bevy_kira_audio::AudioSource;
 
@@ -8,6 +8,29 @@ pub struct LoadingPlugin;
 
 impl Plugin for LoadingPlugin {
     fn build(&self, app: &mut App) {
-        AssetLoader::new(GameState::Loading).build(app);
+        AssetLoader::new(GameState::Loading)
+            .with_collection::<FontAssets>()
+            .with_collection::<AudioAssets>()
+            .with_collection::<TextureAssets>()
+            .continue_to_state(GameState::Menu)
+            .build(app);
     }
+}
+
+#[derive(AssetCollection)]
+pub struct FontAssets {
+    #[asset(path = "fonts/raleway.ttf")]
+    pub raleway: Handle<Font>,
+}
+
+#[derive(AssetCollection)]
+pub struct AudioAssets {
+    #[asset(path = "audio/explosion.ogg")]
+    pub explosion: Handle<AudioSource>,
+}
+
+#[derive(AssetCollection)]
+pub struct TextureAssets {
+    #[asset(path = "icons/bomb.png")]
+    pub bomb: Handle<Image>,
 }
