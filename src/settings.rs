5120644c19595c36b97c57ebaf7a260a13baf918
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-23 17:03:57 +0100

    Added compile time settings

---
+++ src/settings.rs
@@ -0,0 +1,10 @@
+//! Compile time configuration.
+
+/// Window background colour.
+pub const WINDOW_CLEAR_COL: &str = "#A000A0";
+
+/// Window title.
+pub const WINDOW_TITLE: &str = "Minesweeper";
+
+/// Window size [pixels].
+pub const WINDOW_RES: [f32; 2] = [800.0, 600.0];
