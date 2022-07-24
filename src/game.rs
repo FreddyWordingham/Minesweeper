0d2a1cd3fd8525341b5ed49f22faa8fcd4fe523c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-24 16:32:01 +0100

    Added fixed timestep system

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use bevy::{log, prelude::*};
+use bevy::{log, prelude::*, utils::Duration};
 use iyes_loopless::prelude::*;
 
 use crate::{generation::GenerationPlugin, loading::LoadingPlugin};
@@ -15,19 +15,27 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
+        let mut fixed_update = SystemStage::parallel();
+        fixed_update.add_system(Self::test_system_playing.run_in_state(GameState::Playing));
+
         app.add_loopless_state(GameState::Loading)
+            .add_stage_before(
+                CoreStage::Update,
+                "FixedUpdate",
+                FixedTimestepStage::from_stage(Duration::from_millis(1025), fixed_update),
+            )
             .add_plugin(GenerationPlugin)
             .add_plugin(LoadingPlugin)
-            .add_system(Self::test_system)
-            .add_system(Self::test_system_playing.run_in_state(GameState::Playing));
+            .add_system(Self::test_system);
     }
 }
 
 impl GamePlugin {
     #[allow(clippy::needless_pass_by_value)]
-    fn test_system(mut _commands: Commands, keys: Res<Input<KeyCode>>) {
+    fn test_system(mut commands: Commands, keys: Res<Input<KeyCode>>) {
         if keys.just_pressed(KeyCode::Space) {
             log::info!("[SPACED!]");
+            commands.insert_resource(NextState(GameState::Playing));
         }
     }
 
