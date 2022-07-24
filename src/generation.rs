7f41c7c1d2eeb703eb2b1760b2177f45bb69470b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-24 16:23:07 +0100

    Removed old comment

--- src/generation.rs
+++ src/generation.rs
@@ -5,12 +5,6 @@ pub struct GenerationPlugin;
 
 impl Plugin for GenerationPlugin {
     fn build(&self, app: &mut App) {
-        // app.add_system_set(
-        //     ConditionSet::new()
-        //         .run_unless_resource_exists::<Board>()
-        //         .with_system(Self::generate_board)
-        //         .into(),
-        // );
         app.add_system(Self::generate_board.run_unless_resource_exists::<Board>());
     }
 }
