bc3a96e51b830fad37b65f589f3d20874e7597cd
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-27 23:48:24 +0100

    Removed unused button

--- src/menu.rs
+++ src/menu.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use iyes_loopless::prelude::*;
 
-use crate::{game::GameState, loading::FontAssets, resources::Board};
+use crate::{game::GameState, loading::FontAssets};
 
 pub struct MenuPlugin;
 
@@ -18,26 +18,26 @@ impl Plugin for MenuPlugin {
     }
 }
 
-pub struct MenuData {
-    pub play_button: Entity,
-    pub regenerate_button: Entity,
-}
+#[derive(Component)]
+pub struct MenuData;
 
 impl MenuPlugin {
     pub fn spawn_menu(mut commands: Commands, font_assets: Res<FontAssets>) {
-        let play_button = commands
+        commands
             .spawn_bundle(ButtonBundle {
                 style: Style {
                     size: Size::new(Val::Px(120.0), Val::Px(50.0)),
                     margin: Rect::all(Val::Auto),
                     justify_content: JustifyContent::Center,
                     align_items: AlignItems::Center,
+                    flex_direction: FlexDirection::Column,
                     ..default()
                 },
                 color: bevy::prelude::UiColor(Color::GRAY),
                 ..default()
             })
             .insert(Name::new("Start Button"))
+            .insert(MenuData)
             .with_children(|parent| {
                 parent.spawn_bundle(TextBundle {
                     text: Text {
@@ -53,72 +53,25 @@ impl MenuPlugin {
                     },
                     ..default()
                 });
-            })
-            .id();
-
-        let regenerate_button = commands
-            .spawn_bundle(ButtonBundle {
-                style: Style {
-                    size: Size::new(Val::Px(120.0), Val::Px(50.0)),
-                    margin: Rect::all(Val::Auto),
-                    justify_content: JustifyContent::Center,
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                color: bevy::prelude::UiColor(Color::GRAY),
-                ..default()
-            })
-            .insert(Name::new("Regenerate Button"))
-            .with_children(|parent| {
-                parent.spawn_bundle(TextBundle {
-                    text: Text {
-                        sections: vec![TextSection {
-                            value: "Regenerate".to_string(),
-                            style: TextStyle {
-                                font: font_assets.raleway.clone(),
-                                font_size: 40.0,
-                                color: Color::WHITE,
-                            },
-                        }],
-                        alignment: Default::default(),
-                    },
-                    ..default()
-                });
-            })
-            .id();
-
-        commands.insert_resource(MenuData {
-            play_button,
-            regenerate_button,
-        });
+            });
     }
 
-    pub fn despawn_menu(mut commands: Commands, menu_data: Res<MenuData>) {
-        commands.entity(menu_data.play_button).despawn_recursive();
-        commands
-            .entity(menu_data.regenerate_button)
-            .despawn_recursive();
+    pub fn despawn_menu(mut commands: Commands, menu_query: Query<(Entity, &MenuData)>) {
+        for (entity, _) in menu_query.iter() {
+            commands.entity(entity).despawn_recursive();
+        }
     }
 
     fn click_play_button(
         mut commands: Commands,
         mut interaction_query: Query<
-            (Entity, &Name, &Interaction, &mut UiColor),
+            (Entity, &Interaction, &mut UiColor),
             (Changed<Interaction>, With<Button>),
         >,
-        board: ResMut<Board>,
     ) {
-        for (_button, name, interaction, mut color) in interaction_query.iter_mut() {
+        for (_button, interaction, mut color) in interaction_query.iter_mut() {
             match *interaction {
-                Interaction::Clicked => {
-                    if name.as_str() == "Start Button" {
-                        commands.insert_resource(NextState(GameState::Playing))
-                    } else if name.as_str() == "Regenerate Button" {
-                        commands.entity(board.entity).despawn_recursive();
-                        commands.remove_resource::<Board>();
-                        commands.insert_resource(NextState(GameState::Generating))
-                    }
-                }
+                Interaction::Clicked => commands.insert_resource(NextState(GameState::Playing)),
                 Interaction::Hovered => {
                     *color = bevy::prelude::UiColor(Color::PINK);
                 }
